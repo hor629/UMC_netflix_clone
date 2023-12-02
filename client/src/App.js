eb1a104e33071b091d6c99081dfaf97479a5a3c0
hor629 <hor629@naver.com>
2023-12-03 06:30:19 +0900

    [Feat]

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <div className="root-wrap">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Header/>
           <Routes>
           <Route exact path="/" element={<Home/>} />
